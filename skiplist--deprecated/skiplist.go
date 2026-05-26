84ca92835f65f06f8dbcbc3ae271b1a76c9b8a23
vincealwynsibay <v.sibay.139673.tc@umindanao.edu.ph>
2026-05-26 10:47:39 +0800

    feat: remove custom skiplist

---
+++ skiplist--deprecated/skiplist.go
@@ -0,0 +1,83 @@
+package skiplistDep
+
+// import (
+// 	"math"
+// 	"math/rand"
+// )
+
+// type Node struct {
+// 	next  []*Node
+// 	Value int
+// }
+
+// type SkipList struct {
+// 	head     *Node
+// 	maxLevel int
+// }
+
+// var DefaultMaxLevel = 48
+
+// func New(comparable Comparable) *SkipList {
+// 	return &SkipList{
+// 		maxLevel: DefaultMaxLevel,
+// 		head: &Node{
+// 			next:  make([]*Node, DefaultMaxLevel),
+// 			Value: math.MinInt,
+// 		},
+// 	}
+// }
+
+// func (sl *SkipList) Insert(key, value interface{}) *Node {
+// 	current := sl.head
+// 	level := sl.maxLevel - 1
+// 	update := make([]*Node, sl.maxLevel)
+
+// 	// check each level for the last traversed element
+// 	// the last traversed element can possibly be the predecessor depending on nodeLevel luck
+// 	for i := level; i >= 0; i-- {
+// 		for current.next[i] != nil && current.next[i].Value < value {
+// 			current = current.next[i]
+// 		}
+// 		// current is now the rightmost node on level i that is < value
+// 		update[i] = current
+
+// 	}
+
+// 	// test luck, p here is hardcoded as 0.5, make it configurable if needed
+// 	nodeLevel := 1
+// 	for rand.Float64() > 0.5 && nodeLevel < sl.maxLevel {
+// 		nodeLevel++
+// 	}
+
+// 	newNode := &Node{
+// 		next:  make([]*Node, nodeLevel),
+// 		Value: value,
+// 	}
+
+// 	for i := 0; i < nodeLevel; i++ {
+// 		newNode.next[i] = update[i].next[i]
+// 		update[i].next[i] = newNode
+// 	}
+
+// 	return newNode
+// }
+
+// func (sl *SkipList) Search(target int) *Node {
+// 	current := sl.head
+// 	level := sl.maxLevel - 1
+
+// 	for i := level; i >= 0; i-- {
+// 		for current != nil {
+// 			if target > current.Value {
+// 				current = current.next[i]
+// 			} else if target == current.Value {
+// 				return current
+// 			} else {
+// 				current = sl.head
+// 				break
+// 			}
+// 		}
+// 	}
+
+// 	return nil
+// }
